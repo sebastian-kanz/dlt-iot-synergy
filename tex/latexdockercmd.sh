b2213ccd09b09ec541727ea495747c5270f3a7a1
Sebastian Kanz <sebastian.kanz@maibornwolff.de>
2020-06-26 15:10:26 +0200

    reorganizing folder structure

---
+++ tex/latexdockercmd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+IMAGE=blang/latex:ctanfull
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ $machine == "Linux" ]
+then
+    path=$PWD
+else
+	path=$(pwd -W)
+fi
+
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "${path}":/data "$IMAGE" latexmk -C
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "${path}":/data "$IMAGE" pdflatex "$@".tex
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "${path}":/data "$IMAGE" bibtex "$@".aux
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "${path}":/data "$IMAGE" pdflatex "$@".tex
+#docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "${path}":/data "$IMAGE" pdflatex "$@".tex
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "${path}":/data "$IMAGE" latexmk -c
+mv $@.pdf ../
